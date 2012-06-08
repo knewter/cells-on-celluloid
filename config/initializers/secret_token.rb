6e1a966e863f2e9fc5711bfc2625b48ae526ed68
Josh Adams <josh@isotope11.com>
2012-06-08 09:35:54 -0500

    Base rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CellsOnCelluloid::Application.config.secret_token = '1ead25501ba5ecf948cce26010b38f88e76f2a13e8d7b91b714dc7f76b2f23654c4a027a704c811008f60f6bbf672839b18d69830891b38e66becaf0b4b907af'
